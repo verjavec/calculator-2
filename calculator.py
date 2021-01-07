8a8c0fb87ea7051f7048ef1487ea93b6abcdf0f3
Vicki Erjavec <verjavec@gmail.com>
2021-01-07 01:03:21 +0000

    Complete break and add conditionals

--- calculator.py
+++ calculator.py
@@ -7,10 +7,17 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
     # This is a program that runs until we press q
     # Initialize input string variable asks enter equation 
     # Initialize variable tokens as the tokenized version of input string
-    # Develop conditionals for each function
+    # Develop conditionals for each function:
+    # If input_string is 'q', break
+    # If tokens at index 0 is '+'
+    # Call the add function passing in the arguments, integers of tokens[1],[2]
+    
     
 while True:
     input_string = input("Enter equation:  ")
     tokens = input_string.split(' ')
-    print (tokens)
-    break
+    
+    if input_string == 'q':
+        break
+    if tokens[0] == '+':
+        print(add(float(tokens[1]), float(tokens[2])))
